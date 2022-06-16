470e74eff520007d1000a42e335c4f9d93383c1a
Vitaly Engel <vitaliy.engel@gmail.com>
2022-06-16 14:10:25 +0300

    gulpfile build-js added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,39 @@
 const gulp = require("gulp");
+const webpack = require("webpack-stream");
+
+const dist = "/OpenServer/domains/reactAdmin/admin";
 gulp.task("copy-html", () => {
     return gulp.src("app/src/index.html")
         .pipe(gulp.dest("/OpenServer/domains/reactAdmin/admin"))
+});
+gulp.task("build-js", () => {
+    return gulp.src("./app/src/main.js")
+        .pipe(webpack({
+            mode: 'development',
+            output: {
+                filename: 'script.js'
+            },
+            watch: false,
+            devtool: "source-map",
+            module: {
+                rules: [{
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', {
+                                    debug: true,
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }],
+                                "@babel/react"
+                            ]
+                        }
+                    }
+                }]
+            }
+        }))
+        .pipe(gulp.dest(dist))
 });
