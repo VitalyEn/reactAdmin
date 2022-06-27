31f46e7b72e44f31fbf95699399218dd78d858b6
Vitaly Engel <vitaliy.engel@gmail.com>
2022-06-27 15:30:22 +0300

    editor modified

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -30,12 +30,29 @@ export default class Editor extends Component {
             .get(`../${page}`)
             .then(res => this.parseStrToDOM(res.data))
             .then(this.wrapTextNode)
+            .then(dom => {
+                this.virtualDom = dom;
+                return dom;
+            })
             .then(this.serializeDOMToString)
             .then(html => axios.post("./api/saveTempPage.php", {html}))
-       // this.iframe.load(this.currentPage, () => {
-           
-      //  });
+            .then(() => this.iframe.load("../temp.html"))
+            .then(() => this.enableEditing())
       
+    }
+
+    enableEditing(){
+        this.iframe.contentDocument.body.querySelectorAll("text-editor").forEach(element => {
+            element.contentEditable = "true";
+            element.addEventListener("input",() => {
+                this.onTextEdit(element);
+            })
+        })
+    }
+
+    onTextEdit(element){
+        const id = element.getAttribute("nodeid");
+        this.virtualDom.body.querySelector(`[nodeid="${id}"]`).innerHTML = element.innerHTML;
     }
 
     parseStrToDOM(str){
@@ -59,11 +76,12 @@ export default class Editor extends Component {
 
         recursy(body);
 
-        textNodes.forEach(node => {
+        textNodes.forEach((node, i) => {
             const wrapper = dom.createElement('text-editor');
             node.parentNode.replaceChild(wrapper, node);
             wrapper.appendChild(node);
-            wrapper.contentEditable = "true";
+            wrapper.setAttribute("nodeid", i);
+            //wrapper.contentEditable = "true";
         });
 
         return dom;
