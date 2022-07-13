a073eb87ebb1004f55a6a47de2a607d4897d72bb
Vitaly Engel <vitaliy.engel@gmail.com>
2022-07-13 20:35:38 +0300

    editor meta modified

--- app/src/components/editor-meta/editor-meta.js
+++ app/src/components/editor-meta/editor-meta.js
@@ -3,6 +3,19 @@ import React, {Component} from "react";
 export default class EditorMeta extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            title: '',
+            keywords: '',
+            description: ''
+        }
+    }
+
+    componentDidMount(){
+        this.getMeta(this.props.virtualDom);
+    }
+
+    getMeta (virtualDom){
+let title = virtualDom.head.querySelector('title')||virtualDom.head.appendChild(virtualDom.createElement('title'));
     }
 
     render() {
@@ -12,7 +25,17 @@ export default class EditorMeta extends Component {
             <div id={target} data-uk-modal={modal.toString()} container='false'>
                 <div className="uk-modal-dialog uk-modal-body">
                     <h2 className="uk-modal-title">Редактирование Meta-тэгов</h2>
-           
+                    <form>
+                        <div class="uk-margin">
+                            <input class="uk-input" type="text" placeholder="Title"/>
+                        </div>
+                        <div class="uk-margin">
+                            <textarea class="uk-textarea" rows="5" placeholder="Keywords"></textarea>
+                        </div>
+                        <div class="uk-margin">
+                            <textarea class="uk-textarea" rows="5" placeholder="Description"></textarea>
+                        </div>
+                    </form>
                     <p className="uk-text-right">
                         <button className="uk-button uk-button-default uk-margin-small-right uk-modal-close"
                         type="button">Отменить</button>
