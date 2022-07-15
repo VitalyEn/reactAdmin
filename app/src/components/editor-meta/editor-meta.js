061ceee9dd98d37df794078946f9ab48abeda6de
Vitaly Engel <vitaliy.engel@gmail.com>
2022-07-15 14:28:18 +0300

    editorMeta modified

--- app/src/components/editor-meta/editor-meta.js
+++ app/src/components/editor-meta/editor-meta.js
@@ -17,25 +17,46 @@ export default class EditorMeta extends Component {
     }
     
     getMeta(virtualDom) {
-        this.title = virtualDom.head.querySelector('title') || virtualDom.head.appendChild(virtualDom.createElement('title'));
+        let title = virtualDom.head.querySelector('title') || virtualDom.head.appendChild(virtualDom.createElement('title'));
+    
+        let keywords = virtualDom.head.querySelector('meta[name="keywords"]');
+        if(!keywords) {
+            keywords = virtualDom.head.appendChild(virtualDom.createElement('meta'));
+            keywords.setAttribute("name","keywords");
+        }
+
+        let description = virtualDom.head.querySelector('meta[name="description"]');
+        if(!description) {
+            description = virtualDom.head.appendChild(virtualDom.createElement('meta'));
+            description.setAttribute("name","description");
+        }
+
+        this.setState({
+            meta:{
+                title: title.innerHTML,
+                keywords: keywords.getAttribute("content"),
+                description: description.getAttribute("content"),
+            }
+        })
     }
 
     render() {
         const {modal, target} = this.props;
-
+        const {title, keywords, description} = this.state.meta;
+        
         return(
             <div id={target} data-uk-modal={modal.toString()} container='false'>
                 <div className="uk-modal-dialog uk-modal-body">
                     <h2 className="uk-modal-title">Редактирование Meta-тэгов</h2>
                     <form>
                         <div class="uk-margin">
-                            <input class="uk-input" type="text" placeholder="Title"/>
+                            <input className="uk-input" type="text" placeholder="Title" value={title}/>
                         </div>
                         <div class="uk-margin">
-                            <textarea class="uk-textarea" rows="5" placeholder="Keywords"></textarea>
+                            <textarea className="uk-textarea" rows="5" placeholder="Keywords" value={keywords}></textarea>
                         </div>
                         <div class="uk-margin">
-                            <textarea class="uk-textarea" rows="5" placeholder="Description"></textarea>
+                            <textarea className="uk-textarea" rows="5" placeholder="Description" value={description}></textarea>
                         </div>
                     </form>
                     <p className="uk-text-right">
