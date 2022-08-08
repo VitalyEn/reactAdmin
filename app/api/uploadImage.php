5ee5534aeefeaa2cc34a48f427b8a87419d118d7
Vitaly Engel <vitaliy.engel@gmail.com>
2022-08-08 13:23:47 +0300

    uploadImage modified

--- app/api/uploadImage.php
+++ app/api/uploadImage.php
@@ -1,2 +1,9 @@
 <?php
-move_uploaded_file($_FILES["image"]["tmp_name"], "../../img.png");
+
+
+if (file_exists($_FILES["image"]["tmp_name"]) && is_uploaded_file($_FILES["imafe"]["tmp_name"])){
+    $fileExt = explode("/", $_FILES["image"]["type"])[1];
+    $fileName = uniqid().".".$fileExt;
+
+    move_uploaded_file($_FILES["image"]["tmp_name"], "../../img/".$fileName);
+}
