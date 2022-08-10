143bca0ded901f5127d3b6d1795c70ff4ec0dca5
Vitaly Engel <vitaliy.engel@gmail.com>
2022-08-10 13:31:19 +0300

    editor images finished

--- app/src/components/editor-images/editor-images.js
+++ app/src/components/editor-images/editor-images.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 export default class EditorImages {
-    constructor(element, virtualElement, ...cbs) {
+    constructor(element, virtualElement, ...[isLoading, isLoaded, showNotifications]) {
         this.element = element;
         this.virtualElement = virtualElement;
 
         this.element.addEventListener('click', ()=> this.onClick());
         this.imgUploader = document.querySelector("#img-upload");
-        this.isLoading = cbs[0];
-        this.isLoaded = cbs[1];
+        this.isLoading = isLoading;
+        this.isLoaded = isLoaded;
+        this.showNotifications = showNotifications;
     }
 
     onClick() {
@@ -26,6 +27,9 @@ export default class EditorImages {
                     })
                     .then((res)=> {
                         this.virtualElement.src = this.element.src = `./img/${res.data.src}`;
+                    })
+                    .catch(() => this.showNotifications("Ошибка сохранения", "danger"))
+                    .finally(() => {
                         this.imgUploader.value = "";
                         this.isLoaded();
                     })
