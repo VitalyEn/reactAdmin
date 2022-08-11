392c8c170e95cccdda31472b4f159447939d29b3
Vitaly Engel <vitaliy.engel@gmail.com>
2022-08-11 19:10:16 +0300

    login modal render added

--- app/src/components/login/login.js
+++ app/src/components/login/login.js
@@ -4,7 +4,12 @@ export default class Login extends Component {
         return (
             <div className="login-container">
                 <div className="login">
-                    
+                    <h2 className="uk-modal-title uk-text-center">Авторизация</h2>
+                    <div className="uk-margin-top uk-text-lead">Пароль:</div>
+                    <input type="password" name="" id="" className="uk-input uk-margin-top"
+                    placeholder="Пароль"></input>
+                    <button className="uk-button uk-button-primary uk-margin-top"
+                    type="button">Вход</button>
                 </div>
             </div>
         )
